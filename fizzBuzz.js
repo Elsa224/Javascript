92287b5e4348f267524ac44814575096a7e99fa4
Elsa224 <elsazougouri224@gmail.com>
2021-06-17 18:25:16 +0000

    from 20 lines to 14 lines

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -4,17 +4,11 @@ function fizzBuzz()
     count = count + 1;
     if ((count % 3 === 0) && (count % 5 === 0)) 
         output.push("FizzBuzz");
+    else if (count % 3 === 0) 
+        output.push("Fizz");
+    else if (count % 5 === 0) 
+        output.push("Buzz");
     else
-    {
-        if (count % 3 === 0) 
-            output.push("Fizz");
-        else
-        {
-            if (count % 5 === 0) 
-                output.push("Buzz");
-            else
-                output.push(count);
-        }
-    }
+      output.push(count);
     console.log(output);
 }
