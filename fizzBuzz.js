6a271d580b473bc0ab394b3d62a037c1137ce489
Elsa224 <elsazougouri224@gmail.com>
2021-06-17 18:18:57 +0000

    edited two equals to three

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -2,15 +2,15 @@ let output = [], count = 0;
 function fizzBuzz()
 {
     count = count + 1;
-    if ((count % 3 == 0) && (count % 5 == 0)) 
+    if ((count % 3 === 0) && (count % 5 === 0)) 
         output.push("FizzBuzz");
     else
     {
-        if (count % 3 == 0) 
+        if (count % 3 === 0) 
             output.push("Fizz");
         else
         {
-            if (count % 5 == 0) 
+            if (count % 5 === 0) 
                 output.push("Buzz");
             else
                 output.push(count);
